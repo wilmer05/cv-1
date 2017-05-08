4d1ad85d57f778c01d0a051a3228d8058f91d25a
Esther Gonzalez <egonzalez@mahisoft.com>
2017-05-08 18:39:46 +0200

    Add comment to the second part of the lab

--- Lowe_SIFT/homography.m
+++ Lowe_SIFT/homography.m
@@ -1,7 +1,24 @@
+% This function receive two images, compute and return the homography 
 function hg = homography(im1, im2)
-    hg = 0;
+   
+    %The function matcha will compute the sift keypoints for two given images
+    %will return the descriptors for each images, the (row, column, scale,
+    %orientation) in the locaX matrix, the matching points and the quantity
+    %of it
    [desc1 loca1 desc2 loca2 matchings mnb] = match(im1, im2);
+   
+   %This function receive the location of the keypoints and matchings 
+   %found in each image in the previous step and will return two matrix
+   %pts1 and pts2 with the coordinates of the correct matches
    [pts1 pts2] = get_matching_pts(loca1, loca2, matchings);
+   
+   %This variable define the threshold to consider a datapoint a inlier or
+   %not
    threshold = 0.0001;
+   
+   % ransacfithomography will receive the matched keypoints and the defined
+   % threshold. It will find the best model that fit the given data, and it
+   % will return the homography for both images and the points that were
+   % inliers for the final model
    [hg, inliers] = ransacfithomography(pts1, pts2, threshold);
 end
--- Lowe_SIFT/main.m
+++ Lowe_SIFT/main.m
@@ -1,3 +1,6 @@
+%This function receive a folder as parameter
+%It will look for all files which name start with 'im' and will compute the
+%homography for every two pairs or images found in the given folder
 function bla = main(absolute_folder)
     
     files = dir(sprintf('%s/%s', absolute_folder, 'im*'));
@@ -6,13 +9,27 @@ function bla = main(absolute_folder)
     im1 = imread(name_im1);
     for cnt = 2 : total
         name_imc = sprintf('%s/%s', files(cnt).folder, files(cnt).name);
+        
+        %Compute the homography for two images
         H = homography(name_im1, name_imc);
+       
+        %Read the truth homography
         H_truth_file = sprintf('%s/%s', absolute_folder, sprintf('H1to%dp', cnt));
         H_truth = importdata(H_truth_file);
+        
+        %imTrans will transform the image im1 to the corresponding plan
+        %for the previously compute homography H
         [new_im1, bla1] = imTrans(im1, H);
+        
+        %Here the transformation will be compute using the ground truth 
+        %homography
         [new_imc, bla2] = imTrans(im1, H_truth);
+        
+        %Displaying both transformed images
         subplot(1,2,1), imshow(new_im1);
         subplot(1,2,2), imshow(new_imc);
+        
+        %Computing the error using the Frobenius norm
         msg = sprintf('Frobenius error for images %d y %d = %f', 1, cnt, norm(H - H_truth, 'fro'));
         disp(msg);
         pause(2);
